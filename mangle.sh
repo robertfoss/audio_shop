46eee7bd604d89c3376ab8a7d2d4892bd488d563
Robert Foss <robert.foss@collabora.com>
2017-04-16 23:50:40 -0400

    Add video support

--- mangle.sh
+++ mangle.sh
@@ -1,12 +1,25 @@
 #!/usr/bin/env bash
 
+function printDependencies()
+{
+    echo "Error: \"$1\" could not be found, but is required"
+    echo ""
+    echo "This script requires ffmpeg and Sox to be installed"
+
+    exit 1
+}
+
 function printHelp()
 {
-    echo "This script requires ImageMagick, Sox and ffmpeg to be installed"
-    echo ""
     echo "$ ./mangle.sh in.jpg out.png [effect [effect]]"
     echo ""
-    echo "List of effects:"
+    echo "Options:"
+    echo "--bits=X          -- Set audio sample size in bits, 8/16/24"
+    echo "--blend=X         -- Blend the distorted video with original video, 0.5"
+    echo "--color-format=X  -- Color space/format, rgb24/yuv444p/yuyv422. Full list: $ ffmpeg -pix_fmts"
+    echo "--res=WxH         -- Set output resolution, 1920x1080"
+    echo ""
+    echo "Effects:"
     echo "vol 10"
     echo "bass 5"
     echo "sinc 20-4k"
@@ -46,32 +59,125 @@ function helpNeeded()
     fi
 }
 
+function parseArgs()
+{
+    helpNeeded $@
+
+    # Default values
+    BITS=8
+    YUV_FMT=rgb24
+
+    for i in "${@:3}"
+    do
+    case $i in
+        --res=*)
+            export RES=${i#*=}
+            RES_COLON=$(echo $RES | tr x :)
+            FFMPEG_IN_OPTS="$FFMPEG_IN_OPTS -vf scale=$RES_COLON"
+        ;;
+        --bits=*)
+            BITS=${i#*=}
+        ;;
+        --blend=*)
+            BLEND=${i#*=}
+            FFMPEG_OUT_OPTS="$FFMPEG_OUT_OPTS -f rawvideo -pix_fmt \$YUV_FMT -s \${RES} -i /tmp/tmp_audio_in.\${S_TYPE}"
+            FFMPEG_OUT_OPTS="$FFMPEG_OUT_OPTS -filter_complex \\\""
+            FFMPEG_OUT_OPTS="$FFMPEG_OUT_OPTS [0:v]setpts=PTS-STARTPTS, scale=\${RES}[top]\;"
+            FFMPEG_OUT_OPTS="$FFMPEG_OUT_OPTS [1:v]setpts=PTS-STARTPTS, scale=\${RES},"
+            FFMPEG_OUT_OPTS="$FFMPEG_OUT_OPTS format=yuva444p,colorchannelmixer=aa=${BLEND}[bottom]\;"
+            FFMPEG_OUT_OPTS="$FFMPEG_OUT_OPTS [top][bottom]overlay=shortest=1\\\""
+        ;;
+        --color-format=*)
+            YUV_FMT=${i#*=}
+        ;;
+        --*)
+            echo -e "Option $i not recognized\n"
+            printHelp
+        ;;
+        *)
+            # Unknown option, hand them back to SOX
+            SOX_OPTS="$SOX_OPTS $i"
+        ;;
+    esac
+    done
+
+    export BITS
+    export YUV_FMT
+
+    export S_TYPE="u$BITS"
+
+    export FFMPEG_IN_OPTS
+    export FFMPEG_OUT_OPTS
+    export UNUSED_ARGS
+}
+
 function cmd()
 {
-    type $1 >/dev/null 2>&1 || { echo >&2 "$1 is required but it's not installed"; printHelp; exit 1; }
-    eval $@ >/dev/null 2>&1 || { echo >&2 "$@"; eval $@;  exit 1; }
+    OUTPUT=$(eval $@ 2>&1)
+    if [ $? -ne 0 ]; then
+        echo -e "\n----- ERROR -----"
+        echo -e "\n\$ $@\n\n"
+        echo -e "$OUTPUT"
+        echo -e "\n----- ERROR -----"
+        exit 1
+    fi
+    echo "$OUTPUT"
 }
 
-
-function args()
+function cmdSilent()
 {
-    export YUV_FMT=rgb24
-    export BITS=24
-    export S_TYPE=u24
-    export YUV_FMT=yuv444p
-    export BITS=8
-    export S_TYPE=u8
+    OUTPUT=$(eval $@ 2>&1)
+    if [ $? -ne 0 ]; then
+        echo -e "\n----- ERROR -----"
+        echo -e "\n\$ $@\n\n"
+        echo -e "$OUTPUT"
+        echo -e "\n----- ERROR -----"
+        exit 1
+    fi
 }
 
-helpNeeded $@
-args $@
+function getResolution()
+{
+    eval $(cmd ffprobe -v error -of flat=s=_ -select_streams v:0 -show_entries stream=height,width $1)
+    RES=${streams_stream_0_width}${2}${streams_stream_0_height}
+    echo $RES
+}
 
-INFO=$(identify $1 || { echo >&2 "$1 is not a valid image"; exit 1; })
-RES=$(echo $INFO | cut -f3 -d' ' || { echo >&2 "$1 does not have a resolution?"; exit 1; })
-cmd ffmpeg -y -i $1 -pix_fmt $YUV_FMT /tmp/tmp.yuv
-cp /tmp/tmp.yuv /tmp/tmp_audio_in.$S_TYPE
-cmd sox --bits $BITS -c1 -r44100 --encoding unsigned-integer -t $S_TYPE /tmp/tmp_audio_in.$S_TYPE  \
+function getFrames()
+{
+    FRAMES=$(cmd ffprobe -v error -select_streams v:0 -show_entries stream=nb_frames -of default=noprint_wrappers=1:nokey=1 $1)
+    echo $FRAMES
+}
+
+function checkDependencies()
+{
+    for CMD in "$@"
+    do
+        if ! type "$CMD" > /dev/null; then
+            printDependencies $CMD
+        fi
+    done
+}
+
+checkDependencies ffprobe ffmpeg sox tr
+RES=$(getResolution $1 "x")
+parseArgs $@
+FRAMES=$(getFrames $1)
+echo "FFMPEG_IN_OPTS:  $(eval echo $FFMPEG_IN_OPTS)"
+echo "FFMPEG_OUT_OPTS: $(eval echo $FFMPEG_OUT_OPTS)"
+echo "SOX_OPTS:        $(eval echo $SOX_OPTS)"
+
+echo "Extracting raw image data.."
+cmdSilent ffmpeg -y -i $1 -pix_fmt $YUV_FMT $FFMPEG_IN_OPTS /tmp/tmp.yuv
+mv /tmp/tmp.yuv /tmp/tmp_audio_in.$S_TYPE
+
+#echo "Exracting sound.."
+#ffmpeg -y -i $1 -vn -acodec copy /tmp/tmp_in.aac
+echo "Processing as sound.."
+cmdSilent sox --bits $BITS -c1 -r44100 --encoding unsigned-integer -t $S_TYPE /tmp/tmp_audio_in.$S_TYPE  \
         --bits $BITS -c1 -r44100 --encoding unsigned-integer -t $S_TYPE /tmp/tmp_audio_out.$S_TYPE \
-        ${@:3}
-cp /tmp/tmp_audio_out.$S_TYPE /tmp/tmp_out.yuv
-cmd ffmpeg -y -f rawvideo -s $RES -pix_fmt $YUV_FMT -i /tmp/tmp_out.yuv -frames 1 $2
+        $SOX_OPTS
+mv /tmp/tmp_audio_out.$S_TYPE /tmp/tmp_out.yuv
+
+echo "Creating image data from audio.."
+cmdSilent ffmpeg -y "$(eval echo $FFMPEG_OUT_OPTS)" -f rawvideo -pix_fmt $YUV_FMT -s $RES -i /tmp/tmp_out.yuv -frames $FRAMES $2
