ee6ecf9f2b25c4b1960f2a6dd872e030fa6b4da6
Robert Foss <robert.foss@collabora.com>
2017-06-10 21:12:39 -0400

    Add simple batch script

---
+++ batch.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+REPO_DIR=$(dirname $(readlink -f "$0"))
+
+declare -a arr=("bass 5"
+                "echo 0.8 0.88 60 0.4"
+                "echo 0.8 0.88 60 0.4 echo 0.8 0.88 60 0.4"
+                "flanger 0 2 0 71 0.5 25 lin"
+                "hilbert -n 5001"
+                "loudness 6"
+                "norm 90"
+                "norm 45"
+                "norm 15"
+                "overdrive 17"
+                "phaser 0.8 0.74 3 0.7 0.5"
+                "phaser 0.8 0.74 3 0.4 0.5"
+                "pitch 2"
+                "pitch 2 pitch 2"
+                "riaa"
+                "sinc 20-4k"
+                "vol 10"
+)
+
+EXT=${1##*.}
+echo "EXT=$EXT"
+FILE=${1%.*}
+echo "FILE=$FILE"
+for i in "${arr[@]}"
+do
+   EFFECTS=${i// /-}
+   echo "EFFECTS=$EFFECTS"
+   OUTPUT="${FILE}_${EFFECTS}.${EXT}" 
+   echo "OUTPUT=${OUTPUT} "
+   $REPO_DIR/mangle.sh "$1" "${OUTPUT}" --color-format=yuv444p --bits=8 $i
+
+done
