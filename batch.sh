29ba9d60bce3e69ff878940871a3afffd9eff1dd
Robert Foss <robert.foss@collabora.com>
2018-08-15 14:07:22 +0200

    batch: Formatting
    Signed-off-by: Robert Foss <robert.foss@collabora.com>

--- batch.sh
+++ batch.sh
@@ -2,29 +2,30 @@
 
 REPO_DIR=$(dirname $(readlink -f "$0"))
 
-declare -a effects=("bass 5"
-                    "echo 0.8 0.88 60 0.4"
-                    "echo 0.8 0.88 60 0.4 echo 0.8 0.88 60 0.4"
-                    "flanger 0 2 0 71 0.5 25 lin"
-                    "hilbert -n 5001"
-                    "loudness 6"
-                    "norm 90"
-                    "norm 45"
-                    "norm 15"
-                    "overdrive 17"
-                    "phaser 0.8 0.74 3 0.7 0.5"
-                    "phaser 0.8 0.74 3 0.4 0.5"
-                    "pitch 2"
-                    "pitch 2 pitch 2"
-                    "riaa"
-                    "sinc 20-4k"
-                    "vol 10"
+declare -a effects=(
+  "bass 5"
+  "echo 0.8 0.88 60 0.4"
+  "echo 0.8 0.88 60 0.4 echo 0.8 0.88 60 0.4"
+  "flanger 0 2 0 71 0.5 25 lin"
+  "hilbert -n 5001"
+  "loudness 6"
+  "norm 90"
+  "norm 45"
+  "norm 15"
+  "overdrive 17"
+  "phaser 0.8 0.74 3 0.7 0.5"
+  "phaser 0.8 0.74 3 0.4 0.5"
+  "pitch 2"
+  "pitch 2 pitch 2"
+  "riaa"
+  "sinc 20-4k"
+  "vol 10"
 )
 
-declare -a format=("yuv444p"
-                   "rgb24"
-                   "bgr8"
-
+declare -a format=(
+  "yuv444p"
+  "rgb24"
+  "bgr8"
 )
 
 EXT=${1##*.}
